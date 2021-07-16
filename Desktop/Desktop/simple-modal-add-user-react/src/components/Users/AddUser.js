bed3b59bc4259184264805f264016465637ce7e7
Nick Tillinghast <nickt2@cox.net>
2021-07-16 08:35:02 -0600

    added modal functionality

--- Desktop/Desktop/simple-modal-add-user-react/src/components/Users/AddUser.js
+++ Desktop/Desktop/simple-modal-add-user-react/src/components/Users/AddUser.js
@@ -5,33 +5,48 @@ import classes from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = props => {
-    const [enteredUserName, setEnteredUsername] = useState('')
-    const [enteredAge, setEnteredAge] = useState('')
+    const [enteredUserName, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
+
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid input",
+                message: 'Please enter a valid name and age (non-empty values).'
+            })
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: "Invalid age",
+                message: 'Please enter a valid age (> 0).'
+            })
             return;
         }
         props.onAddUser(enteredUserName, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
-    }
+    };
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
 
-    }
+    };
+
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
+    };
+
+    const errorHandler = () => {
+        setError(null);
     }
 
     return (
         <div>
-            <ErrorModal title="An error occured!" message="something went wrong!" />
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">UserName</label>
